86a2bc1d143a20f06e1159db8d717b8c7629a74e
Brian Knapp <brianmknapp@gmail.com>
2020-08-20 07:28:31 -0400

    update

---
+++ docs/build/precache-manifest.c2ba95839a4d8d4ab9a2bbe502603a49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74b4cc326ca5d7c180996e60c3e506a9",
+    "url": "/real-estate-calc/index.html"
+  },
+  {
+    "revision": "d945ce4669403b137615",
+    "url": "/real-estate-calc/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "78b687c4ec9e425a02e1",
+    "url": "/real-estate-calc/static/js/2.3969f49c.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/real-estate-calc/static/js/2.3969f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d945ce4669403b137615",
+    "url": "/real-estate-calc/static/js/main.6d723e1d.chunk.js"
+  },
+  {
+    "revision": "bf9207d2af1955aa8178",
+    "url": "/real-estate-calc/static/js/runtime-main.9769f707.js"
+  }
+]);
